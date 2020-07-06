4e9fd8a8ca46cb940e41943a13797490b51051ba
Pablo Rubianes <pablorubianes@gmail.com>
2020-07-05 23:45:37 -0300

    forgot to save file

--- Hw10/destructuring.js
+++ Hw10/destructuring.js
@@ -3,8 +3,17 @@ Homework #10: Destructuring
 Q: What is the differences between destructuring an object and destructuring an array? 
 When would each be appropriate too use?
 
-A: 
+A: The difference in deconstructing an Array and an Object is the way you have to use to assign the values to variables, 
+Arrays use brackets [] and Objects use Braces {}, 
+also with Objects you have to use the property name from the object 
+to achieve the assignment of the variable.
 
+Other difference is that with Arrays you have to respect the order of the elements that the Array has, 
+so that order is used in the variables assignation, the Objects in the other hand as you 
+use the property name you don't have to respect any order.
 
-
+As for "When would each be appropriate too use?", in my opinion it depends on if you are working with Objects or Arrays, 
+but for ease of use I feel more comfortable to use deconstruction with Objects, 
+because the use of properties prevent you to have to know all the items that an Array has and the order they have.
 */
+
